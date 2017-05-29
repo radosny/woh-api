d6474768077feb2b89877338e3ee2c35d33219a6
Radoslaw Malecki <radoslaw.malecki@grandparade.co.uk>
2017-05-29 21:55:47 +0200

    Refactored parser to obtain unique dates

--- src/dao.js
+++ src/dao.js
@@ -1,20 +1,59 @@
 'use strict';
 
-const Holidays = require('date-holidays');
-const log = require('./log')(module.id);
-const {parseCountries, parseHolidays} = require('./utils');
+const parallel = require('co-parallel');
 
-const hdInstance = new Holidays();
+const log = require('./log')(module.id);
+const api = require('./api');
+const {parseHolidaysResponse} = require('./utils');
+const config = require('./config').get();
+const db = require('monk')(config.DB_URL);
+
+
+function *insertCountry(countries, country) {
+    return (yield countries.insert(country));
+}
+
+exports.getDBInstance = () => db;
+
+exports.createCountries = function *createCountries(countriesList) {
+    log.info('createCountries');
+    const countries = db.create('countries');
+
+    const countriesToInsert = countriesList.map(insertCountry.bind(null, countries));
+    if (countriesToInsert.length > 0) {
+        log.debug('createCountries:remove');
+        yield countries.remove({});
+    }
+    return (yield parallel(countriesToInsert));
+};
 
 exports.getHolidays = function *getHolidays(countryId, year) {
-    log.debug('getHolidays');
-    hdInstance.init(countryId);
-    const holidays = hdInstance.getHolidays(year);
-    return (yield parseHolidays(holidays));
+    const holidays = yield api.getHolidays(countryId, year);
+    const holidaysList = yield parseHolidaysResponse(holidays);
+    return [...holidaysList.keys()];
 };
 
 exports.getCountries = function *getCountries() {
-    log.debug('getCountries');
-    const countries = hdInstance.getCountries();
-    return (yield parseCountries(countries));
+    const countries = db.get('countries');
+    return (yield countries.find({}, '-_id'));
 };
+
+/*
+ const Holidays = require('date-holidays');
+ const log = require('./log')(module.id);
+ const {parseCountries, parseHolidays} = require('./utils');
+ 
+ const hdInstance = new Holidays();
+ 
+ exports.getHolidays = function *getHolidays(countryId, year) {
+ log.debug('getHolidays');
+ hdInstance.init(countryId);
+ const holidays = hdInstance.getHolidays(year);
+ return (yield parseHolidays(holidays));
+ };
+ 
+ exports.getCountries = function *getCountries() {
+ log.debug('getCountries');
+ const countries = hdInstance.getCountries();
+ return (yield parseCountries(countries));
+ };
--- src/utils/holidaysParser.js
+++ src/utils/holidaysParser.js
@@ -9,6 +9,13 @@ exports.parseHolidays = holidays => (
     }, [])
 ));
 
+exports.parseHolidaysResponse = holidays => (
+                                             Promise.resolve(holidays.List.reduce((acc, {occurrences}) => {
+                                                                                  const {0: {day, month, year}} = occurrences;
+                                                                                  return acc.add(new Date(`${year}-${month}-${day} 00:00:00 GMT-0`).toUTCString());
+                                                                                  }, new Set())
+                                                             ));
+
 exports.parseCountries = countries => (
     Promise.resolve(
         map(countries, (country, key) => (
